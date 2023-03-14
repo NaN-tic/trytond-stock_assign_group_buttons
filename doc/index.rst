d2c3de6a533d3039d21e7902775ed66f1518ce2c
Juanjo Garcia <juanjo.garcia@nan-tic.com>
2023-03-14 11:42:53 +0100

    Initial commit

---
+++ doc/index.rst
@@ -0,0 +1,2 @@
+Stock Assign Group Buttons Module
+#################################
---
+++ tests/test_module.py
@@ -0,0 +1,11 @@
+# This file is part stock_assign_group_buttons module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
+from trytond.tests.test_tryton import ModuleTestCase
+
+
+class StockAssignGroupButtonsTestCase(ModuleTestCase):
+    'Test Stock Assign Group Buttons module'
+    module = 'stock_assign_group_buttons'
+
+del ModuleTestCase
