1311cddbf24d3dd80dd28d1faff14c294dde968d
Albert Cervera i Areny <albert@nan-tic.com>
2023-10-11 12:00:16 +0200

    Pyflakes fixes

--- tests/test_module.py
+++ tests/test_module.py
@@ -2,7 +2,6 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.transaction import Transaction
 from trytond.pool import Pool
 
 
